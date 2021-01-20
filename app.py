5e0bccdd6a7f65db7152fbfa5828512c26a1660f
Ayush Subedi <ayush.subedi@gmail.com>
2021-01-20 19:04:30 +0545

    complete simple poc

--- app.py
+++ app.py
@@ -1,13 +1,33 @@
+
+import validators
+import requests
 import click
+from newspaper import Article
+
+
+def summarize(article_text, words):
+    #TODO: add summarizer
+    return article_text[:words]
+
 
 @click.command()
-@click.option('--count', default=1, help='Number of greetings.')
-@click.option('--name', prompt='Your name',
-              help='The person to greet.')
-def hello(count, name):
-    """Simple program that greets NAME for a total of COUNT times."""
-    for x in range(count):
-        click.echo('Hello %s!' % name)
+@click.option('--url', default='https://abc.xyz/', help='Enter a valid URL')
+@click.option('--words', default=150, help='Number of words to summarize to.')
+def get_content(url, words):
+    if not url.startswith('http'):
+        url = 'http://'+url
+        r = requests.get(url)
+        url = r.url
+    if (validators.url(url)):
+        try:
+            article = Article(url, keep_article_html=False)
+            article.download()
+            article.parse()
+            click.echo(summarize(article.text, words))
+        except Exception as e:
+            click.echo(str(e))
+           
+
 
 if __name__ == '__main__':
-    hello()
+    get_content()
