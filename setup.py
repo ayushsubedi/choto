41bf5d811ae996582e57e6913c658a13227f679b
Ayush Subedi <ayush.subedi@gmail.com>
2021-01-21 20:37:55 +0545

    update meta on setup.py for pip

--- setup.py
+++ setup.py
@@ -5,6 +5,11 @@ setup(
     version='1.0',
     py_modules=['term_article_summary'],
     author='Ayush Subedi',
+    author_email='ayush.subedi@gmail.com',
+    description='A CLI tool that summarizes news/articles.',
+    long_description=open('README.md').read(),
+    url='https://github.com/ayushsubedi/term-article-summary',
+    python_requires='>=3.6, <3.9',
     install_requires=[
         'Click',
         'requests',
@@ -18,4 +23,8 @@ setup(
         [console_scripts]
         term_article_summary=term_article_summary:cli
     ''',
+    project_urls={ 
+        'Bug Reports': 'https://github.com/ayushsubedi/term-article-summary/issues',
+        'Source': 'https://github.com/ayushsubedi/term-article-summary',
+    },
 )
